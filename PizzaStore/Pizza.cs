36fc4d8ea118b3b8fefc879c2b8ffa2ea1e1926b
Maskere <Mstachkruse@gmail.com>
2024-03-04 18:23:47 +0100

    Add project files.

---
+++ PizzaStore/Pizza.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaStore
+{
+    public class Pizza
+    {
+        #region Instance Field
+        private int _price;
+        private string _name;
+        #endregion
+
+        #region Constructor
+        public Pizza(string Name, int Price) 
+        {
+            _name = Name;
+            _price = Price;
+        }
+        #endregion
+
+        #region Properties
+        public int Price
+        { 
+            get { return _price; } 
+        }
+        public string Name
+        {
+            get { return _name; }
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString() 
+        {
+            return $"{Name} - Price:{Price}";
+        }
+        #endregion
+    }
+}
---
+++ PizzaStore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PizzaStore 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            BigMamma bigMamma = new BigMamma();
+            bigMamma.Start();
+
+            Console.WriteLine("Press any key to close the program...");
+        }
+    }
+}
